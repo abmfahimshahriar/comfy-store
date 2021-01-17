0ec7052849d3acc30305002cadc669e48e0f96e4
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-17 10:06:36 +0600

    add about and checkout page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,25 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+      <main>
+        <PageHero title="about" />
+        <Wrapper className="page section section-center">
+            <img src={aboutImg} alt="nice desk"/>
+            <article>
+                <div className="title">
+                    <h2>Our story</h2>
+                    <div className="underline"></div>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At iure provident quae
+                    rem repellendus unde voluptate. Ad consectetur culpa deserunt
+                    ipsum iusto nobis odio, perferendis quas, quis, suscipit ullam voluptate.
+                </p>
+            </article>
+        </Wrapper>
+      </main>
+  );
 }
 
 const Wrapper = styled.section`
