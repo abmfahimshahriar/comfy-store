3ce56856dc728e9cb735f7839c5dd8b9730ecf57
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-18 23:38:56 +0600

    add homepage services and contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+      <Wrapper>
+        <div className="section-center">
+          <h3>
+            Join out newsletter and get 20% off
+          </h3>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto blanditiis cupiditate delectus
+              dolor labore minima natus, perspiciatis qui suscipit vel!
+            </p>
+
+            <form className="contact-form">
+              <input type="email" className="form-input" placeholder="enter email"/>
+              <button className="submit-btn">
+                subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </Wrapper>
+  );
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
