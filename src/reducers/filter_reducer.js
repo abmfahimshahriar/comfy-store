1a8108664861c7d843de3f8e76e963f0010eb244
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-26 16:54:31 +0600

    implement filtering functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -83,8 +83,42 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products');
-    return {...state};
+    const {allProducts} = state;
+    const {text, category, company, color, price, shipping} = state.filters;
+    let tempProducts = [...allProducts];
+
+    // filtering with text
+    if(text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().includes(text);
+      });
+    }
+    // filtering with category
+    if (category !== 'all') {
+      tempProducts = tempProducts.filter((product) => product.category === category);
+    }
+
+    // filtering with company
+    if (company !== 'all') {
+      tempProducts = tempProducts.filter((product) => product.company === company);
+    }
+
+    // filtering with color
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+
+    // filtering with price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    // filtering with shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter((product) => product.shipping === true);
+    }
+
+    return {...state, filteredProducts: tempProducts};
   }
 
   if (action.type === CLEAR_FILTERS) {
